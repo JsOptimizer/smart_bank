449fd8e2c25b53bf4bbf8042c6a9013c1ab61536
jsoptimizer <jsoptimizer.pro@gmail.com>
2025-04-17 00:42:18 +0000

    Add custom brand colors to Tailwind configuration and include multiple font files for Apparat family. Remove SpaceMono-Regular font.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,23 @@ module.exports = {
   ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          50: "#e6e6ff",
+          100: "#b1b1fe",
+          200: "#8c8cfe",
+          300: "#5757fd",
+          400: "#3636fd",
+          500: "#0404fc",
+          600: "#0404e5",
+          700: "#0303b3",
+          800: "#02028b",
+          900: "#02026a",
+          950: "#050533",
+        },
+      },
+    },
   },
   plugins: [],
 };
